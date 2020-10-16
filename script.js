35881df4275e423115733c82222af6eacc6cb982
wolfdog27 <kylenicholasanderson@gmail.com>
2020-10-16 13:15:38 -0700

    working hasSpecialCharacters confirm

--- script.js
+++ script.js
@@ -3,55 +3,75 @@ var generateBtn = document.querySelector("#generate");
 
 // Global variables for function
 // Split function automatically puts them into an array.
-var possibleCharacters = [];
 var specialCharacters = "!@#$%^&*()?><+-".split("");
 var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbers = "1234567890".split("");
+
+var possibleCharacters = [];
 
 // console.log(specialCharacters);
 // console.log(lowerCaseCharacters);
 // console.log(upperCaseCharacters);
 // console.log(numbers);
 
-var hasSpecialCharacters = true;
-var hasLowerCaseLetters = true;
-var hasUpperCaseLetters = true;
-var hasNumbers = true;
+var hasSpecialCharacters;
+var hasLowerCaseLetters;
+var hasUpperCaseLetters;
+var hasNumbers;
+
+// main function
+function generatePassword() {
+
+  // Prompt user for desired characters and convert string into a number
+  var passwordLength = parseInt(prompt("How many characters would you like your password to be?  Must be a number between 8 and 128."));
+  // loop over input and make sure the input is a number between 8-128 and it not a letter or symbol
+  while (passwordLength <= 7 || passwordLength >= 129 || isNaN(passwordLength)) {
+    alert("Please pick a valid number between 8 and 128");
+    var passwordLength = parseInt(prompt("How many characters would you like your password to be?  Must be a number between 8 and 128."));
+  }
+
+  var hasSpecialCharacters = confirm("Would you like to use Special Characters in your password?");
+  if (hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    console.log(possibleCharacters);
+  }
+  
 
 
-function generatePassword () {
-var passwordLength = parseInt(prompt("How many characters would you like your password to be?  Must be a number between 8 and 128."));
+  // let hasSpecialCharacters = confirm("Would you like your password to include Special Characters?");
+  // if (hasSpecialCharacters) {
+  //   possibleCharacters.concat(lowerCaseCharacters);
+  // }
+  // console.log(possibleCharacters);
 
-if (passwordLength<=7 || passwordLength>=129 || isNaN(passwordLength)) {
-  alert ("Please pick a valid number between 8 and 128");
-  passwordLength;
 
-} else {
-  // rest of promts
+
+
+
 }
 
-console.log(passwordLength);
 
 
-}
+
+
 
 // Generate password:
 // prompt user for Password length
 // Need to store password length in variable
 // need to iterate that many times to store password
-  // confirm whether user wants: special characters, numbers, Upper Case, Lowercase
-  // hasSpecialChar true/false variable * upper, lower, num 
+// confirm whether user wants: special characters, numbers, Upper Case, Lowercase
+// hasSpecialChar true/false variable * upper, lower, num 
 
 // if character boolean is true, add array to possibleChar array - concact or spread opporator
 
-  // we have array, then we add more stuff to array depending on answers
+// we have array, then we add more stuff to array depending on answers
 
-  // once we have choices in array, then build password
-  // loop over length of array, grab random character from array, 
+// once we have choices in array, then build password
+// loop over length of array, grab random character from array, 
 
-  // var passwordStr=""
-  // return passwordStr and become password
+// var passwordStr=""
+// return passwordStr and become password
 
 // Write password to the #password input
 function writePassword() {
