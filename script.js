27c268dbdb51f86a6451731ad3e3911a9b6eefe5
wolfdog27 <kylenicholasanderson@gmail.com>
2020-10-16 13:33:50 -0700

    edge case for choices

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ var possibleCharacters = [];
 function generatePassword() {
 
 
-  // Prompt user for desired characters and convert string into a number
+  // Prompt user for desired password length and convert string into a number
   var passwordLength = parseInt(prompt("How many characters would you like your password to be?  Must be a number between 8 and 128."));
   // loop over input and make sure the input is a number between 8-128 and it not a letter or symbol
   while (passwordLength <= 7 || passwordLength >= 129 || isNaN(passwordLength)) {
     alert("Please pick a valid number between 8 and 128");
-    var passwordLength = parseInt(prompt("How many characters would you like your password to be?  Must be a number between 8 and 128."));
+    passwordLength = parseInt(prompt("How many characters would you like your password to be?  Must be a number between 8 and 128."));
   }
 
   // confirm special characters
@@ -51,13 +51,36 @@ function generatePassword() {
     console.log(possibleCharacters);
   }
 
+  while (!hasSpecialCharacters && !hasLowerCaseLetters && !hasUpperCaseLetters && !hasNumbers) {
+    alert("You use at least one of the following: Special Characters, Lower Case Letters, Upper Case Letters, or Numbers")
+    // confirm special characters
+    var hasSpecialCharacters = confirm("Would you like to use Special Characters in your password?");
+    if (hasSpecialCharacters) {
+      possibleCharacters = possibleCharacters.concat(specialCharacters);
+      console.log(possibleCharacters);
+    }
 
-  // let hasSpecialCharacters = confirm("Would you like your password to include Special Characters?");
-  // if (hasSpecialCharacters) {
-  //   possibleCharacters.concat(lowerCaseCharacters);
-  // }
-  // console.log(possibleCharacters);
+    // confirm lower case letters
+    var hasLowerCaseLetters = confirm("Would you like to use Lower Case Letters in your password?");
+    if (hasLowerCaseLetters) {
+      possibleCharacters = possibleCharacters.concat(lowerCaseLetters);
+      console.log(possibleCharacters);
+    }
 
+    // confirm upper case letters
+    var hasUpperCaseLetters = confirm("Would you like to use Upper Case Letters in your password?");
+    if (hasUpperCaseLetters) {
+      possibleCharacters = possibleCharacters.concat(upperCaseLetters);
+      console.log(possibleCharacters);
+    }
+
+    // confirm lower case letters
+    var hasNumbers = confirm("Would you like to use numbers in your password?");
+    if (hasNumbers) {
+      possibleCharacters = possibleCharacters.concat(numbers);
+      console.log(possibleCharacters);
+    }
+  }
 
 
 
