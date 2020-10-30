04abd9291e5fa058d6d9e0a121b041b34ff57f3f
wolfdog27 <kylenicholasanderson@gmail.com>
2020-10-30 14:52:35 -0700

    completed password generator

--- script.js
+++ script.js
@@ -82,32 +82,14 @@ function generatePassword() {
     }
   }
 
-
-
-
+  // take in input and make password
+  var makePassword = [];
+  for (var i = 0; i < passwordLength; i++) {
+    var index = Math.floor(Math.random() * possibleCharacters.length)
+    makePassword.push(possibleCharacters[index])
+  }
+  return makePassword.join("")
 }
-
-
-
-
-
-
-// Generate password:
-// prompt user for Password length
-// Need to store password length in variable
-// need to iterate that many times to store password
-// confirm whether user wants: special characters, numbers, Upper Case, Lowercase
-// hasSpecialChar true/false variable * upper, lower, num 
-
-// if character boolean is true, add array to possibleChar array - concact or spread opporator
-
-// we have array, then we add more stuff to array depending on answers
-
-// once we have choices in array, then build password
-// loop over length of array, grab random character from array, 
-
-// var passwordStr=""
-// return passwordStr and become password
 
 // Write password to the #password input
 function writePassword() {
